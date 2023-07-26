38001cbd86a220a104ec761e5620891d18ce3927
Eshan Shikarkhane <108228113+eshan-one@users.noreply.github.com>
2023-07-26 22:15:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-const searchBtn = document.getElementById('search-button')
+const searchField = document.getElementById('search-input')
+const searchBtn =  document.getElementById('search-button')
 const movieContainer = document.getElementById('movie-container')
-const searchInput = document.getElementById('search-input')
-let imdbID 
+const apiKey =  `dac3b8e3`
+const apiURL =  `https://www.omdbapi.com/?apikey=${apiKey}&type=movie`
+let imdbId 
+
+const  moviesArr = []
+
+searchBtn.addEventListener('click', getMoviesID)
 
 async function getMovieData(id){
-    const res =  await fetch(`https://www.omdbapi.com/?i=${id}&apikey=dac3b8e3`);
-    const data  = await res.json()
+
+    const response  = await fetch(`${apiURL}&i=${id}`)
+    const movieData =  await  response.json()
+    const {Title, Plot, Poster} =  movieData
     
-    const {Title, Year, Runtime, Genre, Director, Actors, Plot, Poster} = data
-
-    const Object = {
-        title: Title,
-        image: Poster,
-        runtime: Runtime,
-        plot: Plot,
-    }
-
-    movieContainer.innerHTML  +=  `<div class="movie">
+    if(Poster !== 'N/A' && Plot !== 'N/A'){
+        moviesArr.push(movieData)
+    
+    movieContainer.innerHTML += `<div class="movie">
             <div class="movie-img" id="">
-                <img id="movie-img" src="${Object.image}" alt="Movie poster">
+                <img id="movie-img" src="${Poster}" alt="Movie poster">
             </div>
             <div class="movie-data" id="movie-data">
-                <h4 class="movie-title" id="movie-title">${Object.title}</h4>
-                <p class="movie-plot" id="movie-plot">${Object.plot}</p>
+                <h4 class="movie-title" id="movie-title">${Title}</h4>
+                <p class="movie-plot" id="movie-plot">${Plot}</p>
             </div>
         </div>
         <hr>`
-}
-
-function findMovies(){
-    let search =  (searchInput.value)
-    return search
     
 }
 
-
-searchBtn.addEventListener('click', async()=>{
-    
-    let s =  'maze runner'
-
-    const res =  await fetch(`https://www.omdbapi.com/?s=${s}&apikey=dac3b8e3`)    
-    const data = await res.json()
-    for(let i=0; i < 5; i++){
-        imdbID  = data.Search[i].imdbID
-        getMovieData(imdbID)   
     }
-     
-})
-  
-
+    
+async function getMoviesID(){
+    try{
+        const apiRes =  await(await fetch(`${apiURL}&s=${searchField.value}`)).json()
+        const results =  apiRes.Search
+        if(!results){
+            console.log('none-found')
+        }
+        else{
+            results.forEach(result =>{
+                imdbId =  result.imdbID
+                getMovieData(imdbId)
+            })
+        }
+    }
+    
+    catch(e){
+        console.log('handle network errors', e)
+    }
+    
+}
+// javascript
